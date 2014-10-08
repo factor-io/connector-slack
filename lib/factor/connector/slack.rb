bb65a28333f0f8a8ec31fa276b9b1f544f3c53b1
Andrew Akers <andrewrdakers@gmail.com>
2014-10-08 12:23:30 -0700

    should start a listener checking for token and channel

--- lib/factor/connector/slack.rb
+++ lib/factor/connector/slack.rb
@@ -2,6 +2,42 @@ require 'factor-connector-api'
 require 'rest_client'
 
 Factor::Connector.service 'slack' do
+  %w(message notification exit enter topic_change).each do |listener_name|
+    listener listener_name do
+      start do |params|
+        channel     = params['channel']
+        token       = params['token']
+        filter      = params['filter']
+        listener_id = channel
+
+        fail 'Token is required' unless token
+        fail 'Channel is required' unless channel
+        fail 'Filter is required' if !filter && listener_name == 'message'
+
+        hook_url = get_web_hook(listener_id)
+
+        base = 'https://slack.com/api/channels.history'
+        auth = "?token=#{token}&"
+        chan = "channel=#{channel}"
+        uri  = base + auth + chan
+
+        headers = {
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json'
+        }
+
+        info 'Getting hooks'
+        begin
+          http_response = RestClient.post { uri, body: {}.to_json, headers: headers)
+        rescue
+          fail 'Could not get list of hooks. Check your creds.'
+        end
+      end
+    end
+  end
+
+
+
   action "send" do |params|
 
     token   = params['token']
@@ -21,9 +57,9 @@ Factor::Connector.service 'slack' do
 
     info "Posting message `#{text}` to channel #{channel}"
     begin
-      url = 'https://slack.com/api/chat.postMessage'
-      raw_response = RestClient.post(url, payload)
-      response = JSON.parse(raw_response)
+      uri          = 'https://slack.com/api/chat.postMessage'
+      raw_response = RestClient.post(uri, payload)
+      response     = JSON.parse(raw_response)
     rescue
       fail 'Unable to deliver your message'
     end
