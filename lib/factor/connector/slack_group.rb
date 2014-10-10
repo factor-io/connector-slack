2fc4c1a4fe7b2a55b66c333bb73b6aa2a91ed881
Andrew <andrewrdakers@gmail.com>
2014-10-10 14:54:26 -0700

    invite a user to channel

--- lib/factor/connector/slack_group.rb
+++ lib/factor/connector/slack_group.rb
@@ -28,4 +28,34 @@ Factor::Connector.service 'slack_group' do
 
     action_callback response
   end
+
+  action "invite" do |params|
+
+    token      = params['token']
+    channel_id = params['channel_id']
+    user_id    = params['user_id']
+
+    fail 'Token is required' unless token
+    fail 'Channel is required' unless channel
+    fail 'Username is required' unless user
+
+    payload = {
+      token:   token,
+      channel: channel_id,
+      user:    user_id
+    }
+
+    info "Inviting #{user} to group #{channel}"
+    begin
+      uri          = 'https://slack.com/api/groups.invite'
+      raw_response = RestClient.post(uri, payload)
+      response     = JSON.parse(raw_response)
+    rescue
+      fail 'Unable to invite user'
+    end
+
+    fail "Error from Slack API: #{response['error']}" unless response['ok']
+
+    action_callback response
+  end
 end
--- spec/slack_group_spec.rb
+++ spec/slack_group_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
+token = ENV['token']
+
 describe 'slack' do
   it 'can create a private group' do
-
-    token = ENV['token']
 
     service_instance = service_instance('slack_group')
 
