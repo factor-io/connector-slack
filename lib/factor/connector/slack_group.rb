b663ca08b6103ac209f85ede1a76b33d67bad6c1
Andrew <andrewrdakers@gmail.com>
2014-10-10 11:07:56 -0700

    create new private group

---
+++ lib/factor/connector/slack_group.rb
@@ -0,0 +1,30 @@
+require 'factor-connector-api'
+require 'rest_client'
+
+Factor::Connector.service 'slack_group' do
+  action "create" do |params|
+
+    token = params['token']
+    name  = params['name']
+
+    fail 'Token is required' unless token
+    fail 'Name is required' unless name
+
+    payload = {
+      token: token,
+      name:  name,
+    }
+
+    info "Creating group `#{name}`"
+    begin
+      uri          = 'https://slack.com/api/groups.create'
+      raw_response = RestClient.post(uri, payload)
+      response     = JSON.parse(raw_response)
+    rescue
+      fail 'Unable to create group'
+    end
+
+    fail "Error from Slack API: #{response['error']}" unless response['ok']
+
+    action_callback response
+  end
