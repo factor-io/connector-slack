e9322cd555e564501710593e134d56d83e7f66a0
Andrew <andrewrdakers@gmail.com>
2014-10-10 10:39:00 -0700

    upload file to slack

--- lib/factor/connector/slack.rb
+++ lib/factor/connector/slack.rb
@@ -31,5 +31,35 @@ Factor::Connector.service 'slack' do
 
     action_callback response
   end
-end
 
+  action "upload" do |params|
+    token   = params['token']
+    channel = params['channel']
+    file    = params['file']
+    content = params['content'] || File.open(file, 'rb').read
+
+    fail 'Token is required' unless token
+    fail 'Channel is required' unless channel
+    fail 'File is required' unless file
+
+    body = {
+      token:   token,
+      channel: channel,
+      file:    file,
+      content: content
+    }
+
+    info "Uploading File `#{file}` to channel #{channel}"
+    begin
+      uri          = 'https://slack.com/api/files.upload'
+      raw_response = RestClient.post(uri, body)
+      reponse      = JSON.parse(raw_response)
+    rescue
+      fail 'Unable to upload you file'
+    end
+
+    fail "Error from Slack API: #{response['error']}" unless response['ok']
+
+    action_callback response
+  end
+end
