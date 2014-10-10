a8f63de837ba3c0fc52292f482df4e3e9bba9b96
Andrew <andrewrdakers@gmail.com>
2014-10-10 10:40:03 -0700

    test upload file

--- spec/slack_spec.rb
+++ spec/slack_spec.rb
@@ -1,20 +1,40 @@
 require 'spec_helper'
 
-describe 'Slack' do
+describe 'slack' do
   it 'can send a message' do
 
     token = ENV['token']
-    channel  = ENV['channel']
+
+    service_instance = service_instance('slack')
+
+    params = {
+      'token' => token,
+      'channel' => '#general',
+      'text' => 'whatup'
+    }
+
+    service_instance.test_action('send', params) do
+      expect_info
+    end
+  end
+
+  it 'can upload a file' do
+
+    token = ENV['token']
+    file = ENV['file']
+    channel = ENV['channel']
+    content = File.open(file, 'rb').read
 
     service_instance = service_instance('slack')
 
     params = {
       'token' => token,
       'channel' => channel,
-      'text' => 'hey'
+      'file' => file,
+      'content' => content
     }
 
-    service_instance.test_action('send',params) do
+    service_instance.test_action('upload', params) do
       expect_info
     end
   end
