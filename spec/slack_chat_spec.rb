3a59c8623efa6440592b2d16b502f58dd8f487f1
Andrew Akers <andrewrdakers@gmail.com>
2014-10-14 12:36:50 -0700

    move test for postMessage

---
+++ spec/slack_chat_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'slack' do
+
+  before(:all) do
+    @token = ENV['SLACK_TOKEN']
+    @channel = ENV['SLACK_CHANNEL']
+    @text = ENV['SLACK_TEXT']
+    @user = ENV['SLACK_USER']
+  end
+
+  it 'can send a message' do
+
+    service_instance = service_instance('slack_chat')
+
+    params = {
+      'token' => @token,
+      'channel' => '#general',
+      'text' => @text
+    }
+
+    service_instance.test_action('send', params) do
+      expect_info message: "Posting Message"
+    end
+  end
+end
