f6fca124f3c6a0d358e1321e0b39b80214ab8e22
Andrew Akers <andrewrdakers@gmail.com>
2014-10-15 13:50:02 -0700

    look for members and channel from name

--- lib/factor/connector/slack_group.rb
+++ lib/factor/connector/slack_group.rb
@@ -31,13 +31,51 @@ Factor::Connector.service 'slack_group' do
 
   action "invite" do |params|
 
-    token      = params['token']
-    channel    = params['channel']
-    user       = params['user']
+    token   = params['token']
+    channel = params['channel']
+    user    = params['user']
 
     fail 'Token is required' unless token
     fail 'Channel is required' unless channel
     fail 'Username is required' unless user
+
+    payload = {
+      token: token
+    }
+
+    info "Getting all Channels"
+    begin
+      uri          = 'https://slack.com/api/channels.list'
+      raw_response = RestClient::Request.execute(url:uri, method:'POST', ssl_version:'SSLv23', payload:payload)
+      response     = JSON.parse(raw_response)
+    rescue
+      fail "Failed to connect to Slack API, check your credentials"
+    end
+
+    fail response['error'] unless response['ok']
+
+    response['channels'].each do |n|
+      if n['name'] == channel
+        channel = n['id']
+      end
+    end
+
+    info "Getting Users"
+    begin
+      uri          = 'https://slack.com/api/users.list'
+      raw_response = RestClient::Request.execute(url:uri, method:'POST', ssl_version:'SSLv23', payload:payload)
+      response     = JSON.parse(raw_response)
+    rescue
+      fail "Failed to connect to Slack API, check your credentials"
+    end
+
+    fail response['error'] unless response['ok']
+
+    response['members'].each do |n|
+      if n['name'] == user
+        user = n['id']
+      end
+    end
 
     payload = {
       token:   token,
