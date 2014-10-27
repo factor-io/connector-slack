d59a652af666051565d67c32cde6d53c28236a2a
Andrew Akers <andrewrdakers@gmail.com>
2014-10-27 13:06:27 -0700

    channel_name to channel_id conversion

--- lib/factor/connector/slack_chat.rb
+++ lib/factor/connector/slack_chat.rb
@@ -4,17 +4,43 @@ require 'rest_client'
 Factor::Connector.service 'slack_chat' do
   action 'send' do |params|
 
-    token   = params['token']
-    channel = params['channel']
-    text    = params['text']
+    token        = params['token']
+    channel_name = params['channel']
+    text         = params['text']
 
     fail 'Token is required' unless token
-    fail 'Channel is required' unless channel
+    fail 'Channel is required' unless channel_name
     fail 'Text is required' unless text
 
     payload = {
+      token: token
+    }
+
+    info 'Getting all Channels'
+    begin
+      uri          = 'https://slack.com/api/channels.list'
+      raw_response = RestClient::Request.execute(
+                                          url: uri,
+                                          method: 'POST',
+                                          ssl_version: 'SSLv23',
+                                          payload: payload
+                                        )
+      response     = JSON.parse(raw_response)
+    rescue
+      fail 'failed to connect to Slack API, check your credentials'
+    end
+
+    fail response['error'] unless response['ok']
+
+    channel = response['channels'].find { |c| c['name'] || c['id'] == channel_name }
+
+    fail "Channel '#{channel_name}' wasn't found" unless channel
+
+    channel_id = channel['id']
+
+    payload = {
       token:   token,
-      channel: channel,
+      channel: channel_id,
       text:    text
     }
 
